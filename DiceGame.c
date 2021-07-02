caa500bdff572865a0681726c51b91d89f96bc39
SUZUKI Yasuyuki <yasuyuki200047@gmail.com>
2021-07-02 11:41:12 +0900

    Won/lost message added

--- DiceGame.c
+++ DiceGame.c
@@ -4,7 +4,11 @@
 int main(){
   int d1,d2;
   int total;
+  char name[20];
   
+  printf("What is your name?\n");
+  scanf("%s",name);
+  printf("Hello, %s\n",name);
   printf("Rolling the dice...\n");
   d1=rand()%6+1;
   d2=rand()%6+1;
@@ -12,5 +16,11 @@ int main(){
   printf("Die 2: %d\n",d2);
   total=d1+d2;
   printf("Total value: %d\n",total);
+  if(total>7){
+    printf("%s won\n",name);
+  }
+  else {
+    printf("%s lost\n",name);
+  }
   return 0;
 }
